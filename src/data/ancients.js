0a2139db62bc36d3e8dca30f440726542149d2f2
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2022-08-28 21:37:16 +0300

    feat: add JS logic

--- src/data/ancients.js
+++ src/data/ancients.js
@@ -1,86 +1,86 @@
-import Ancients from '../assets/Ancients/index'
+import Ancients from "../assets/Ancients/index";
 
 const ancientsData = [
   {
-    id: 'azathoth',
-    name: 'azathoth',
+    id: "azathoth",
+    name: "azathoth",
     cardFace: Ancients.azathoth,
     firstStage: {
       greenCards: 1,
-      blueCards: 1,
       brownCards: 2,
+      blueCards: 1,
     },
     secondStage: {
       greenCards: 2,
-      blueCards: 1,
       brownCards: 3,
+      blueCards: 1,
     },
     thirdStage: {
       greenCards: 2,
-      blueCards: 0,
       brownCards: 4,
+      blueCards: 0,
     },
   },
   {
-    id: 'cthulhu',
-    name: 'cthulhu',
+    id: "cthulthu",
+    name: "cthulthu",
     cardFace: Ancients.cthulhu,
     firstStage: {
       greenCards: 0,
-      blueCards: 2,
       brownCards: 2,
+      blueCards: 2,
     },
     secondStage: {
       greenCards: 1,
-      blueCards: 0,
       brownCards: 3,
+      blueCards: 0,
     },
     thirdStage: {
       greenCards: 3,
-      blueCards: 0,
       brownCards: 4,
+      blueCards: 0,
     },
   },
   {
-    id: 'iogSothoth',
-    name: 'iogSothoth',
+    id: "iogSothoth",
+    name: "iogSothoth",
     cardFace: Ancients.iogSothoth,
     firstStage: {
       greenCards: 0,
-      blueCards: 1,
       brownCards: 2,
+      blueCards: 1,
     },
     secondStage: {
       greenCards: 2,
-      blueCards: 1,
       brownCards: 3,
+      blueCards: 1,
     },
     thirdStage: {
       greenCards: 3,
-      blueCards: 0,
       brownCards: 4,
+      blueCards: 0,
     },
   },
   {
-    id: 'shubNiggurath',
-    name: 'shubNiggurath',
+    id: "shubNiggurath",
+    name: "shubNiggurath",
     cardFace: Ancients.shubNiggurath,
     firstStage: {
       greenCards: 1,
-      blueCards: 1,
       brownCards: 2,
+      blueCards: 1,
     },
     secondStage: {
       greenCards: 3,
-      blueCards: 1,
       brownCards: 2,
+      blueCards: 1,
     },
     thirdStage: {
       greenCards: 2,
-      blueCards: 0,
       brownCards: 4,
+      blueCards: 0,
     },
   },
-]
+];
 
-export default ancientsData
+export default ancientsData;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,51 @@ import "./index.html";
 import "./index.scss";
 import { mult, sum } from "./modules/calc";
 import ancientsData from "./data/ancients";
+import greenCards from "./data/mythicCards/green";
+import blueCards from "./data/mythicCards/blue";
+import brownCards from "./data/mythicCards/brown";
+
+const ancients = document.querySelector(".ancients");
+const gameCards = document.querySelectorAll(".game-card");
+
+const fillCards = (stageArr) => {
+  gameCards.forEach((item, index) => {
+    item.textContent = stageArr[index];
+  });
+};
+
+const fillStage = (ancient) => {
+  let stage = [];
+  for (let value in ancient) {
+    if (typeof ancient[value] === "object") {
+      stage.push(
+        ancient[value].greenCards,
+        ancient[value].brownCards,
+        ancient[value].blueCards
+      );
+    }
+  }
+  return stage;
+};
+
+const takeStages = (selectedHero) => {
+  for (let ancient of ancientsData) {
+    if (ancient.id === selectedHero) {
+      let stageArr = fillStage(ancient);
+      fillCards(stageArr);
+    }
+  }
+};
+
+ancients.addEventListener("click", (e) => {
+  console.log(e.target.id);
+  takeStages(e.target.id);
+});
 
 const imgWrap = document.querySelector(".card-result");
 const img = new Image();
-img.src = ancientsData[1].cardFace;
+img.src = greenCards[1].cardFace;
+// img.src = ancientsData[1].cardFace;
 imgWrap.append(img);
 
 const ul = document.querySelector("ul");
