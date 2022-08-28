b3f891a019599d57002067f98fb00011254f9915
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2022-08-29 01:30:21 +0300

    feat: word 1 deck

--- src/index.js
+++ src/index.js
@@ -8,14 +8,19 @@ import brownCards from "./data/mythicCards/brown";
 
 const ancients = document.querySelector(".ancients");
 const gameCards = document.querySelectorAll(".game-card");
+const shuffleBtn = document.querySelector(".shuffle-btn");
+const cardBtn = document.querySelector(".card-backside");
+shuffleBtn.disabled = true;
 
-const fillCards = (stageArr) => {
+// заполнили схему
+const fillScheme = (stageArr) => {
   gameCards.forEach((item, index) => {
     item.textContent = stageArr[index];
   });
 };
 
-const fillStage = (ancient) => {
+// заполнили массив всех нужных карт на игру
+const fillAllStages = (ancient) => {
   let stage = [];
   for (let value in ancient) {
     if (typeof ancient[value] === "object") {
@@ -29,26 +34,192 @@ const fillStage = (ancient) => {
   return stage;
 };
 
-const takeStages = (selectedHero) => {
+// заполнили конкретный стейдж рандомными картами и для рандома отсортировали их по последней цифре
+const fillStage = (stage, greenCards, brownCards, blueCards) => {
+  const arr1 = addGreenCard(stage[0], greenCards);
+  const arr2 = addBrownCard(stage[1], brownCards);
+  const arr3 = addBlueCard(stage[2], blueCards);
+
+  return [arr1, arr2, arr3].flat().sort(function (a, b) {
+    if (a.id.slice(-1) > b.id.slice(-1)) {
+      return 1;
+    }
+    if (a.id.slice(-1) < b.id.slice(-1)) {
+      return -1;
+    }
+    return 0;
+  });
+};
+
+// получили сумму карт
+const getCardAmount = (color) => {
+  const cardsNodes = document.querySelectorAll(`.game-card--${color}`);
+  const cards = Array.from(cardsNodes);
+  const result = cards.reduce((acc, item) => {
+    return acc + +item.textContent;
+  }, 0);
+  return result;
+};
+
+// функция для рандома числа
+const getRandomIntInclusive = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+// получаем массив рандомных карточек
+const getCardsArrayByColor = (color, num) => {
+  const copyGreenCards = [...greenCards];
+  const copyBrownCards = [...brownCards];
+  const copyBlueCards = [...blueCards];
+
+  const resulCardsArr = [];
+
+  if (color === "green") {
+    for (let i = 0; i < num; i++) {
+      const randomNum = getRandomIntInclusive(0, copyGreenCards.length);
+      const randomCard = copyGreenCards.splice(randomNum, 1);
+      resulCardsArr.push(...randomCard);
+    }
+  } else if (color === "brown") {
+    for (let i = 0; i < num; i++) {
+      const randomNum = getRandomIntInclusive(0, copyBrownCards.length);
+      const randomCard = copyBrownCards.splice(randomNum, 1);
+      resulCardsArr.push(...randomCard);
+    }
+  } else if (color === "blue") {
+    for (let i = 0; i < num; i++) {
+      const randomNum = getRandomIntInclusive(0, copyBlueCards.length);
+      const randomCard = copyBlueCards.splice(randomNum, 1);
+      resulCardsArr.push(...randomCard);
+    }
+  }
+
+  return [...resulCardsArr];
+};
+
+const addGreenCard = (count, greenCards) => {
+  const result = [];
+
+  for (let i = 0; i < count; i++) {
+    const randomNum = getRandomIntInclusive(0, greenCards.length);
+    const item = greenCards.splice(randomNum, 1);
+    result.push(...item);
+  }
+
+  return result;
+};
+
+const addBrownCard = (count, brownCards) => {
+  const result = [];
+
+  for (let i = 0; i < count; i++) {
+    const randomNum = getRandomIntInclusive(0, brownCards.length);
+    const item = brownCards.splice(randomNum, 1);
+    result.push(...item);
+  }
+
+  return result;
+};
+
+const addBlueCard = (count, blueCards) => {
+  const result = [];
+
+  for (let i = 0; i < count; i++) {
+    const randomNum = getRandomIntInclusive(0, blueCards.length);
+    const item = blueCards.splice(randomNum, 1);
+    result.push(...item);
+  }
+
+  return result;
+};
+
+// наполняем колоду
+const fillDeck = (stagesArr) => {
+  // количество карт (число) на все этапы по цветам
+  const greenAmountCards = getCardAmount("green");
+  const browmAmountCards = getCardAmount("brown");
+  const blueAmountCards = getCardAmount("blue");
+
+  // массивы рандомных карт (объектов) по цветам в суммарном для игры количетсве
+  const arrOfRndGreenCards = getCardsArrayByColor("green", greenAmountCards);
+  const arrOfRndBrownCards = getCardsArrayByColor("brown", browmAmountCards);
+  const arrOfRndBlueCards = getCardsArrayByColor("blue", blueAmountCards);
+
+  // количество карт (число) по цветам в этапах
+  const firstStage = [stagesArr[0], stagesArr[1], stagesArr[2]];
+  const secondStage = [stagesArr[3], stagesArr[4], stagesArr[5]];
+  const thirdStage = [stagesArr[6], stagesArr[7], stagesArr[8]];
+
+  // заполняем stage 1
+  const filledFirstStage = fillStage(
+    firstStage,
+    arrOfRndGreenCards,
+    arrOfRndBrownCards,
+    arrOfRndBlueCards
+  );
+  // заполняем stage 1
+  const filledSecondStage = fillStage(
+    secondStage,
+    arrOfRndGreenCards,
+    arrOfRndBrownCards,
+    arrOfRndBlueCards
+  );
+  // заполняем stage 1
+  const filledThirdStage = fillStage(
+    thirdStage,
+    arrOfRndGreenCards,
+    arrOfRndBrownCards,
+    arrOfRndBlueCards
+  );
+
+  return [
+    ...filledFirstStage,
+    ...filledSecondStage,
+    ...filledThirdStage,
+  ].flat();
+};
+
+const imgWrap = document.querySelector(".card-result");
+const img = new Image();
+imgWrap.append(img);
+let counter = 0;
+
+const startShuffleCards = (deck) => {
+  cardBtn.addEventListener("click", (e) => {
+    if (counter < deck.length) {
+      img.src = deck[counter].cardFace;
+      counter++;
+      console.log("click");
+    }
+  });
+};
+
+const startPreparing = (selectedHero) => {
   for (let ancient of ancientsData) {
     if (ancient.id === selectedHero) {
-      let stageArr = fillStage(ancient);
-      fillCards(stageArr);
+      let stagesArr = fillAllStages(ancient);
+      fillScheme(stagesArr);
+      const deck = fillDeck(stagesArr);
+      console.log(deck);
+      counter = 0;
+
+      startShuffleCards(deck);
     }
   }
 };
 
+const addActiveClass = (item) => {
+  const targetClass = item.classList[0];
+  document.querySelectorAll(`.${targetClass}`).forEach((item) => {
+    item.classList.remove("active");
+  });
+
+  item.classList.add("active");
+};
+
 ancients.addEventListener("click", (e) => {
-  console.log(e.target.id);
-  takeStages(e.target.id);
+  addActiveClass(e.target);
+  startPreparing(e.target.id);
 });
-
-const imgWrap = document.querySelector(".card-result");
-const img = new Image();
-img.src = greenCards[1].cardFace;
-// img.src = ancientsData[1].cardFace;
-imgWrap.append(img);
-
-const ul = document.querySelector("ul");
-console.log(ul);
-// ul.style.background = `url('${ancientsData[1].cardFace}')`;
